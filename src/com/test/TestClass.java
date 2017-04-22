0fee488265ad1978ea71e30af7a7456b3a34fb93
Jagadeesh <jagadeesh.cheluvaraj@gmail.com>
2017-04-22 20:14:33 +0530

    FirstClass commit

---
+++ src/com/test/TestClass.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * Created by jagadeesh on 22/04/17.
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        System.out.printf("Test Class running");
+
+    }
+
+}
